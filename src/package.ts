0c3ef5da479d3edfe9a6e9bd425e99d91bccba9e
TED Vortex <ted.vortex@gmail.com>
2022-02-06 21:05:16 +0200

    feat: add local cache option as part of the pairing test

--- src/package.ts
+++ src/package.ts
@@ -2,8 +2,51 @@ import { RequestHandler } from 'express';
 import { ls } from 'npm-remote-ls';
 import { treeify } from 'json-toy';
 import got from "got";
+import { readFileSync, writeFileSync, mkdirSync, statSync } from 'fs';
 
 import { NPMPackage } from './types';
+
+const offset = 5 * 60 * 1000;
+
+const checkCache = (name, version) => {
+  try {
+    const now = new Date().getTime();
+    const file = JSON.parse(readFileSync(`./.cache/${name}/${version}/manifest.json`, 'utf8'));
+
+    const diff = now - new Date(file.lastCached).getTime();
+
+    return diff > offset ? false : file;
+  } catch (e) {
+    return false;
+  }
+}
+
+const checkDirExists = (name) => {
+  try {
+    statSync(`./cache/${name}`);
+
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+const cacheBody = (name, version, body) => {
+  try {
+    if (!checkDirExists(name)) {
+      mkdirSync(`./.cache/${name}/${version}`, {
+        recursive: true
+      });
+    }
+
+    writeFileSync(`./.cache/${name}/${version}/manifest.json`, body);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+
+  return true;
+}
 
 const als = (name, version) =>
   new Promise((resolve, reject) => {
@@ -21,6 +64,22 @@ const als = (name, version) =>
  */
 export const getPackage: RequestHandler = async function (req, res, next) {
   const { name, version, html } = req.params;
+  const now = new Date();
+
+  const cacheManifest = checkCache(name, version);
+  delete cacheManifest.lastCached;
+
+  if (cacheManifest) {
+    console.log('cached response');
+    if (html === undefined) {
+      return res.status(200).json(cacheManifest);
+    } else {
+      return res.status(200).render('tree', {
+        title: `${name}@${version}`,
+        data: treeify(cacheManifest),
+      });
+    }
+  }
 
   try {
     // reinstate exact registry fetch as als doesn't actually throw
@@ -34,6 +93,11 @@ export const getPackage: RequestHandler = async function (req, res, next) {
       version,
       dependencies,
     };
+
+    cacheBody(name, version, JSON.stringify({
+      ...body,
+      lastCached: now.toISOString(),
+    }));
 
     if (html === undefined) {
       return res.status(200).json(body);
